8e0339de273a1f9eae3b6dc5d1913f9ec3f955b7
sam song <sasong@ucsc.edu>
2017-03-15 17:40:42 -0700

    time is wokring

--- radial.js
+++ radial.js
@@ -3,7 +3,7 @@
   CruzID: sasong
   CMPS161 Proj2
 
-  The program takes in 1 file consisting of 
+  The program takes in 1 csv to produce a cyclic streamgraph.
 */
 
 
@@ -17,42 +17,119 @@
 // variable to represent svg 
 var svg = d3.select("svg"),
     width = +svg.attr("width"),
-    height = +svg.attr("height"),
+    height = +svg.attr("height");
     streamG = svg.append("sg");
 
+var csvPath = "dataT.csv";
+
 var angle = d3.scaleLinear()
-    .range([0, 2* Math.PI])
+    .range([0, 2* Math.PI]);
+
+var angleT = d3.scaleTime()
+    .range([0, 2*Math.PI])
+    //.domain([new Date, new Date])
+    //.nice(d3.timeDay);
+// parses time read in to time format.
+var parseT = d3.timeFormat("%Y-%m-%d %H:%M:%S").parse;
+var parseT1 = d3.timeFormat('%H:%M');
 
 var radius = d3.scaleLinear()
     //.domain([d3.min(layers, stackMin), d3.max(layers, stackMax)])
-    .range([height*3/4, height*1/4 ]);
+    .range([height*1/2, height*1/16 ]);
 
 var z = d3.interpolateCool;
 
+// Stack data
+var n = 0; // number of layers
+var m = 0; // number of samples per layer
+var layerArr = [];
+var layers;
+var maxS = 0;
+var minS = 0;
+var labelColor = [];
+var timeStamps = [];
+//Important, each row is one "signal"
+var startDay = "1-1-2000 ";
+var maxT = new Date(startDay);
+var minT = new Date(startDay);
+
+
+function stackMax(layer) {
+  return d3.max(layer, function(d) { return d[1]; });
+}
+
+function stackMin(layer) {
+  return d3.min(layer, function(d) { return d[0]; });
+}
+
+
 // Input read
 
+// Read in Data
+var inputData; //raw data from d3.csv
+var legend; // legend for each each
 // first function parses data
 // second function operates upon the parsed data
-var inputData;
-d3.csv("data.csv", function(d, i, columns) 
+d3.csv("dataT.csv", function(d, i, columns) 
 {
   //console.log(columns);
   for (i = 1, t = 0; i < columns.length; ++i){
     t += d[columns[i]] = +d[columns[i]];
-    //console.log(d[columns[i]]);
   }
-  d.total = t;
-  //console.log(d);
+  d.total = t; 
   return d;
 }, function(error, data) 
 {
-	console.log(data);
+	//console.log(data);
 	inputData = data;
-	elements = data.columns;
-	angle.domain(data.map(function(d){return d[elements[0]]})); // gets the domain for each element
-	console.log(data.map(function(d){return d[elements[0]]}));
-	radius.domain([0, d3.max(data, function(d) { return d.total; })]);
-});
+	var elements = data.columns;
+
+	m = data.length; // number of samples per layer
+	n = elements.length - 1; // number of layers
+	legend = data.columns.slice(1);
+	angle.domain([0, m-1]); // sets the angular domain
+  //angleT.format(d3.time.format("%H:%M"));
+
+	//var stack = d3.stack().keys(d3.range(1)).offset(d3.stackOffsetWiggle)
+	//angle.domain = [0, m - 1];
+
+  var times = data.map(function(d){return d[elements[0]]});
+
+  //presumes 24hour format
+  for(var i = 0; i < times.length; i++)
+  {
+    //console.log(maxT)
+    var t1 = new Date(startDay + times[i]);
+    console.log(t1);
+    var t = d3.timeParse(times[i]);
+    timeStamps.push(t1);
+  }
+
+  minT = d3.min(timeStamps);
+
+  maxT = d3.max(timeStamps);
+  
+  angleT.domain[minT, maxT];
+  // Parse the data into stacks
+  // first column is ignored as that it marks the time
+  // parses each individual column as a seperate layer
+	for(var i = 1; i < n+1; i++)
+	{
+		var row = data.map(function(d){return d[elements[i]]}); // gets all the data for a single layer
+		layerArr.push(row);
+	}
+  var stack = d3.stack()
+      .keys(d3.range(n))
+      .offset(d3.stackOffsetWiggle)
+      .order(d3.stackOrderInsideOut) 
+
+  layers = stack(d3.transpose( layerArr));
+
+  radius.domain([d3.min(layers, stackMin), d3.max(layers, stackMax)]);
+
+  findColorLabels(legend);
+})
+
 
 // calculates the area in a radial version
 var areaR = d3.radialArea()
@@ -61,25 +138,55 @@ var areaR = d3.radialArea()
     .innerRadius(function(d) { return radius(d[0]); })
     .outerRadius(function(d) { return radius(d[1]); });
 
+
+// Creates 
+var areaRT = d3.radialArea()
+    .curve(d3.curveCardinalClosed)
+    .angle(function(d, i) { return angleT(timeStamps[i]); })
+    .innerRadius(function(d) {return radius(d[0]); })
+    .outerRadius(function(d) { return radius(d[1]); });
+
+// Returns a random integer between min and max
+function randInt(min, max)
+{
+  return Math.floor(Math.random() * (max - min)) + min + 1;
+}
+
+function randColor()
+{
+  var color;
+  // 16777215 = ffffff from decimal to hexidecimal
+  color = randInt(0, 16777215);
+
+  //convert to hexidecimal
+  return '#' + color.toString(16);
+}
+
+// gives each element within the legend a color
+function findColorLabels(set)
+{
+  setColor = [];
+  for(i in set)
+  {
+    labelColor.push(randColor())
+  }
+}
+
+
+
 // add time label to the function
 // var axesLabel = 
 // var 
-function main1()
+function load()
 {
+	console.log("loading");
 	// whats been ran
 	svg.selectAll("path")
-  		.data(layers0) // data
+  		.data(layers) // data
   		.enter().append("path")
     		.attr("class", "layer")
-    		.attr("d", areaR)
+    		.attr("d", areaRT)
+        //.attr("d", areaRT)
     		.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
-    		//.style("fill", function(d,i) { return color(Number(d.key.substring(1))); })
-
-    		//.attr("d", area)
-    		.attr("fill", function() { return z(Math.random()); }); // color?
-    		//.attr("fill", function(){ return z(250)})
-};
-
-function main(){
-	document.write(inputData);
+    		.style("fill", function(d,i) { return labelColor[i]})
 };
--- radialStreamgraph.js
+++ radialStreamgraph.js
@@ -3,6 +3,27 @@ var n = 20, // number of layers
     m = 200, // number of samples per layer
     k = 10; // number of bumps per layer
 
+
+  var data = [
+    [1, 4, 2, 7, 9, 15], 
+    [1, 2, 5, 10, 12, 19],
+    [1, 20, 16, 6, 10, 5],
+    [1, 17, 16, 6, 10, 20],
+    [1, 17, 16, 60, 10, 10],
+    [1, 17, 16, 6, 10, 30],
+    [1, 17, 80, 6, 100, 30]
+  ];
+
+  data = data.map(function (d) {
+    return d.map(function (p, i) {
+        return {
+            x: i,
+            y: p,
+            y0: 0
+        };
+    });
+  });
+
 // nShifts the baseline so as to minimize the weighted wiggle of layers. This offset is recommended for streamgraphs in 
 // conjunction with the inside-out order. See Stacked Graphs—Geometry & Aesthetics by Bryon & Wattenberg for more information.
 // stackkeys(d3.range(n)) presumably produces a key for each element within the range
@@ -11,6 +32,8 @@ var stack = d3.stack().keys(d3.range(n)).offset(d3.stackOffsetWiggle),
     layers1 = stack(d3.transpose(d3.range(n).map(function() { return bumps(m, k); }))),
     layers = layers0.concat(layers1);
 
+var stack1 = d3.stack().offset(d3.stackOffsetWiggle),
+    layers2 = stack1(data);
 
 var svg = d3.select("svg"),
     width = +svg.attr("width"),
@@ -33,7 +56,7 @@ var z = d3.interpolateCool;
 
 var areaR = d3.radialArea()
     .curve(d3.curveCardinalClosed)
-    .angle(function(d, i) { return angle(i); })
+    .angle(function(d, i) { console.log(i),  console.log(d[i]); return angle(i); })
     .innerRadius(function(d) { return radius(d[0]); })
     .outerRadius(function(d) { return radius(d[1]); });
 
@@ -63,7 +86,7 @@ function stackMin(layer) {
 function transition() {
   var t;
   d3.selectAll("path")
-    .data((t = layers1, layers1 = layers0, layers0 = t)) // transision function switches layers
+    .data((t = layers2, layers2 = layers0, layers0 = t)) // transision function switches layers
     .transition()
       .duration(500)
       .attr("d", areaR);
@@ -72,8 +95,10 @@ function transition() {
 // Inspired by Lee Byron’s test data generator.
 function bumps(n, m) {
   var a = [], i;
-  for (i = 0; i < n; ++i) a[i] = 0;
+  for (i = 0; i < n; ++i) a[i] = 0; // initiate every layer to 0
+  console.log(a.length);
   for (i = 0; i < m; ++i) bump(a, n);
+
   return a;
 }
 
@@ -85,4 +110,6 @@ function bump(a, n) {
     var w = (i / n - y) * z;
     a[i] += x * Math.exp(-w * w);
   }
+
+  //console.log(a);
 }
